2dc2107385b6023980dd50445ae2377acf6ef0dc
a-dulene21 <camilakengne21@gmail.com>
2026-07-01 04:09:32 +0200

    add file dockerfie

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from backend.formular import form_bp  # Import the blueprint from the module where it's defined
 from flask_cors import CORS
-from backend.created_filter_car import filter_bp
+from backend.server import filter_bp
 
 
 app = Flask(__name__)
@@ -13,13 +13,13 @@ app.register_blueprint(filter_bp)
 
 
 if __name__ == "__main__":
-    print("Flask Backend läuft auf http://localhost:5000/submit_form")
-    print("Flask Backend läuft auf http://localhost:5000/cars")
-    print("Flask Backend läuft auf http://localhost:5000/recommend")
+    print("Flask Backend läuft auf http://localhost:5001/submit_form")
+    print("Flask Backend läuft auf http://localhost:5001/cars")
+    print("Flask Backend läuft auf http://localhost:5001/recommend")
 
 
 
     print('Hallo')
 
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
 
